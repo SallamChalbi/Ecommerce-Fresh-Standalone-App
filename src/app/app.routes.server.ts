fdf7607f7444263bb7d8530d43b85c9930b7b3ef
SallamChalbi <139037169+SallamChalbi@users.noreply.github.com>
2025-08-05 19:27:58 +0100

    add RenderMode.Server to  app.route.server

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'productdetails/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'categorydetails/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
