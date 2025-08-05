6f24e9373febb6a2c77a82e46a02c7a3a168a895
SallamChalbi <139037169+SallamChalbi@users.noreply.github.com>
2025-08-05 19:35:29 +0100

    add RenderMode.Server(payment/:id) to  app.route.server

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -6,6 +6,10 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Server
   },
   {
+    path: 'payment/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: 'categorydetails/:id',
     renderMode: RenderMode.Server
   },
